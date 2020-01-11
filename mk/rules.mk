ca8a5f167ddf0eeb43bb62395ff656de52c5dfcd
LewisCollum <lewiscollum4@gmail.com>
2020-01-10 23:11:55 -0500

    Added rule for GDB debugging

--- mk/rules.mk
+++ mk/rules.mk
@@ -9,12 +9,17 @@ test_runner:
 	cxxtestgen --error-printer -o $@.cpp $(TESTS)
 	$(CXX) $(CXXFLAGS) -o $@ $@.cpp -Icxxtest
 
+test_runner_debug:
+	cxxtestgen --error-printer -o $@.cpp $(TESTS)
+	$(CXX) -std=c++2a -g -o $@ $@.cpp -Icxxtest
+
 test: test_runner
 	-./test_runner
 	$(RM) test_runner test_runner.cpp
 
-testKeep: test_runner
-	./test_runner
+testDebug: test_runner_debug
+	-gdb ./test_runner_debug
+	$(RM) test_runner_debug test_runner_debug.cpp
 
 clean:
 	$(RM) -r $(BUILD_DIR) test_runner*
