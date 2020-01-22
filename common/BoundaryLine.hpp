faa2366b787f43734029d3a627950884c94b055d
LewisCollum <lewiscollum4@gmail.com>
2020-01-22 01:05:50 -0500

    Removed constexpr construction of BoundaryLine

--- common/BoundaryLine.hpp
+++ common/BoundaryLine.hpp
@@ -19,21 +19,19 @@ public:
     auto const end() { return boundaryLine.end(); }
 
 private:
-    constexpr void makeBoundaryLine() requires isHorizontal {
+    void makeBoundaryLine() requires isHorizontal {
         for (int i = 0; i < length; ++i) {
-            constexpr LateralCoordinates lateralCoordinates{
+            boundaryLine[i] = {
                 {origin.x + i, origin.y},
                 {origin.x + i, origin.y+1}};
-            boundaryLine[i] = lateralCoordinates;
         }
     }
     
-    constexpr void makeBoundaryLine() requires isVertical {
+    void makeBoundaryLine() requires isVertical {
         for (int i = 0; i < length; ++i) {
-            boundaryLine[i]{LateralCoordinates{
-                    {1,1},{1,1}}};
-                // {origin.x, origin.y + i},
-                // {origin.x+1, origin.y + i}};
+            boundaryLine[i] = {
+                {origin.x, origin.y + i},
+                {origin.x+1, origin.y + i}};
         }
     }    
 };
--- common/test/TestArea.hpp
+++ common/test/TestArea.hpp
@@ -8,79 +8,32 @@
 
 template <RectangularLimits limits>
 class Area {
-    constexpr int width() {
+    static constexpr int width() {
         return limits.x.second - limits.x.first + 1;
     }
 
-    constexpr int height() {
+    static constexpr int height() {
         return limits.y.second - limits.y.first + 1;
     }
         
-    VerticalBoundaryLine<limits.y.second - limits.y.first + 1> westBoundaryLine{{limits.x.first-1, limits.y.first}};
-    // VerticalBoundaryLine const eastBoundaryLine;
-    // HorizontalBoundaryLine const northBoundaryLine;
-    // HorizontalBoundaryLine const southBoundaryLine;
+    VerticalBoundaryLine<height()> westBoundaryLine;
+    VerticalBoundaryLine<height()> eastBoundaryLine;
+    HorizontalBoundaryLine<width()> northBoundaryLine;
+    HorizontalBoundaryLine<width()>  southBoundaryLine;
     
 public:
-
-    // constexpr Area(RectangularLimits limits) :
-    //     limits{limits},
-    //     westBoundaryLine{makeWestBoundaryLine()},
-    //     eastBoundaryLine{makeEastBoundaryLine()},
-    //     northBoundaryLine{makeNorthBoundaryLine()},
-    //     southBoundaryLine{makeSouthBoundaryLine()} {}
-    
-
-    auto boundaryLinesWithinLimits(RectangularLimits otherLimits) {
-        std::tuple boundaryLines{};
-        // if(bool isWest = this->limits.x.first > limits.x.first)            
-        //     std::tuple_cat(boundaryLines, westBoundaryLine);
-
-        return boundaryLines;
-    }
-private:
-    // PointMagnitude makeWestLine() {
-    //     return {
-    //       origin: {x:limits.x.first-1, y:limits.y.first},
-    //       magnitude: 1 //height()
-    //     };
-    // }
-
-
-    // constexpr VerticalBoundaryLine makeEastBoundaryLine() const {
-    //     constexpr PointMagnitude eastLine{
-    //       origin: {x: limits.x.second-1, y: limits.y.first},
-    //       magnitude: height()
-    //     };
-
-    //     return VerticalBoundaryLine{eastLine};
-    // }
-
-    // constexpr HorizontalBoundaryLine makeNorthBoundaryLine() const {
-    //     constexpr PointMagnitude northLine{
-    //       origin: {x: limits.x.first, y: limits.y.first-1},
-    //       magnitude: width()
-    //     };
-
-    //     return HorizontalBoundaryLine{northLine};
-    // }
-
-    // constexpr HorizontalBoundaryLine makeSouthBoundaryLine() const {
-    //     constexpr PointMagnitude southLine{
-    //       origin: {x: limits.x.second, y: limits.y.second-1},
-    //       magnitude: width()
-    //     };
-
-    //     return HorizontalBoundaryLine{southLine};
-    // }    
+    constexpr Area() :
+        westBoundaryLine{{x: limits.x.first-1, y: limits.y.first}},
+        eastBoundaryLine{{x: limits.x.second-1, y: limits.y.first}},
+        northBoundaryLine{{x: limits.x.first, y: limits.y.first-1}},
+        southBoundaryLine{{x: limits.x.second, y: limits.y.second-1}} {}   
 };
 
 class TestArea : public CxxTest::TestSuite {
-    constexpr static RectangularLimits const areaLimits{x: {0,2}, y: {0,3}};
-    constexpr static Area<areaLimits> area{};
+    static constexpr RectangularLimits areaLimits{x: {0,2}, y: {0,3}};
+    Area<areaLimits> area{};
 
 public:
-    void test_() {}
     // void test_boundaryLinesWithinMinimumLimits_noBoundaryLines() {
     //     RectangularLimits coordinateLimits = areaLimits;
     //     auto boundaryLines = area.boundaryLinesWithinLimits(coordinateLimits);
@@ -95,7 +48,7 @@ public:
     //     RectangularLimits coordinateLimits = areaLimits;
     //     auto boundaryLines = area.boundaryLinesWithinLimits(coordinateLimits);
 
-    //     auto expected = 0;
+    //     auto expected = 1;
     //     auto actual = std::tuple_size<decltype(boundaryLines)>::value;
 
     //     TS_ASSERT_EQUALS(expected, actual);
